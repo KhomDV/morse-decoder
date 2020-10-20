bb9bd6227229d40e67b2c353eaa9d13908c34bd1
KhomDV <KhomDV@mail.ru>
2020-10-20 20:40:24 +0300

    feat: Solution task morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,29 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+	let str = '';
+	while (true) {
+		const codeStr = expr.substring(0, 10);
+		if ( codeStr === "**********" ) {
+			str = str + " ";
+		} else {
+			let codeMorse = '';
+			for (i=1; i<6; i++) {
+				const simbol = codeStr.substring((i*2)-2, (i*2));
+				if ( simbol === '10' ) {
+					codeMorse = codeMorse + '.';
+				} else if ( simbol === '11' ) {
+					codeMorse = codeMorse + '-';
+				}	
+			}
+			str = str + MORSE_TABLE[codeMorse];
+		}	
+		expr = expr.substring(10);
+		if ( expr.length === 0 ) {
+			break;
+		}
+	}
+    return str;
 }
 
 module.exports = {
